69bdd1f37a3bce39a80734094391b78711b70509
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2022-10-12 08:07:43 +0300

    practice comments changed

--- practice/practice2/ex6.py
+++ practice/practice2/ex6.py
@@ -1,10 +1,10 @@
-"""
-Даны два слова. Напечатать только те буквы слов,  которые встречаются в обоих словах только один раз.
-"""
+#
+# Даны два слова. Напечатать только те буквы слов,  которые встречаются в обоих словах только один раз.
+#
 
 words1 = input()
 words2 = input()
-common_letters = set(words1) | set(words2)
+common_letters = set(words1) & set(words2)
 for letter in common_letters:
     if words1.count(letter) == 1 and words2.count(letter) == 1:
         print(letter)
